64ddf448e49a783e1162d791c494f831094ede68
MissaelRodriguez <Blackphantom@gmail.com>
2015-01-15 14:03:26 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -1,42 +1,44 @@
-$("document").ready(function(){
-   //$("div p:last-child").css("background-color", "red");
-   
-   // $("div p:first-child").css("background-color", "yellow");
-   
-   $("div:nth-child(1)").css("background-color", "lightgray");
-   
-   $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
-   
-   $('h1').bind('click', mouseClick);
-   
-   $('#show').css('visibility', 'hidden');
-   
-   $('#hide').bind('click', hideThePage);
-   
-   $('#show').bind('click', showThePage);
+$("document").ready(function() {
+    //$("div p:last-child").css("background-color", "red");
+
+    // $("div p:first-child").css("background-color", "yellow");
+
+    $("div:nth-child(1)").css("background-color", "lightgray");
+
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+
+    $('h1').bind('click', mouseClick);
+
+    $('#show').css('visibility', 'hidden');
+
+    $('#hide').bind('click', hideThePage);
+
+    $('#superHumans').accordion({header: "h3"});
+
+    $('#show').bind('click', showThePage);
 
 });
 
-function hideThePage(){
+function hideThePage() {
     $('#show').css('visibility', 'visible');
     $('div').hide('slide', {}, 2500);
     $('#show').show('fold', {}, 2500);
 }
 
-function showThePage(){
+function showThePage() {
     $('div').show('fold', {}, 2500);
     $('#show').hide('puff', {}, 2500);
 }
 
-function mouseOverMe(){
+function mouseOverMe() {
     $("h1").html("Here");
 }
 
-function mouseOutMe(){
+function mouseOutMe() {
     $('h1').html('Click');
 }
 
-function mouseClick(){
+function mouseClick() {
     $('p').html('Clicked!!!!!!!!!!!!!!!!');
 }
 
@@ -50,14 +52,14 @@ $('#randPara').bind('click', addAPara);
 
 $('#removePara').bind('click', removeAPara);
 
-function removeAPara(){
+function removeAPara() {
     $('#randPara p:last').remove();
 }
 
-function addAPara(){
+function addAPara() {
     $('#randPara').append('<p>HI!</p>');
 }
 
-function replaceWText(){
+function replaceWText() {
     $('#replaceWText').text('More pointless randome text!');
 }
