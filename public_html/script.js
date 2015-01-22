d3c1bd550dc60c018e72c45a3678915abf7ee9da
MissaelRodriguez <Blackphantom@gmail.com>
2015-01-22 13:11:25 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -9,7 +9,7 @@ $("document").ready(function() {
     $('#show').css('visibility', 'hidden');
 
     $('#hide').bind('click', hideThePage);
-
+//lets the accordion do what it was told to do
     $('#superHumans').accordion({header: "h3"});
 
     $('#show').bind('click', showThePage);
@@ -21,22 +21,22 @@ function hideThePage() {
     $('div').hide('slide', {}, 2500);
     $('#show').show('fold', {}, 2500);
 }
-
+//shows the page when you click a button
 function showThePage() {
     $('div').show('fold', {}, 2500);
     $('#show').hide('puff', {}, 2500);
 }
 
 function mouseOverMe() {
-    $("h1").html("Here");
+    $("h1").html("Last");
 }
 
 function mouseOutMe() {
-    $('h1').html('Click');
+    $('h1').html('Here');
 }
-
+//shows "Clicked!" when you hit a button
 function mouseClick() {
-    $('p').html('Clicked!!!!!!!!!!!!!!!!');
+    $('p').html('Clicked!!!!!!!!');
 }
 
 $('#replaceWText').bind('click', replaceWText);
@@ -48,11 +48,11 @@ $('#removePara').bind('click', removeAPara);
 function removeAPara() {
     $('#randPara p:last').remove();
 }
-
+//adds new text
 function addAPara() {
     $('#randPara').append('<p>Now Click Number 1!</p>');
 }
-
+//changes text to something else
 function replaceWText() {
     $('#replaceWText').text('More pointless text!');
 }
